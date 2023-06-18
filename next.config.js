84a530e306716da261fa1578528c2e6f976141e4
Vartikaj <38675130+Vartikaj@users.noreply.github.com>
2023-06-18 07:27:04 +0530

    chnages

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const nextConfig = {
 
 module.exports = {
   eslint: {
-    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['.'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
 }
